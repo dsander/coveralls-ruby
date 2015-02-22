a062c892e8ee4505d37881603b2375c4c7a47b0d
Dominik Sander <git@dsander.de>
2015-02-22 22:30:23 +0100

    ENV['COVERALLS_ENDPOINT'] just sets the API_HOST not API_DOMAIN

--- lib/coveralls/api.rb
+++ lib/coveralls/api.rb
@@ -5,12 +5,11 @@ module Coveralls
   class API
     if ENV['COVERALLS_ENDPOINT']
       API_HOST = ENV['COVERALLS_ENDPOINT']
-      API_DOMAIN = ENV['COVERALLS_ENDPOINT']
     else
       API_HOST = ENV['COVERALLS_DEVELOPMENT'] ? "localhost:3000" : "coveralls.io"
-      API_PROTOCOL = ENV['COVERALLS_DEVELOPMENT'] ? "http" : "https"
-      API_DOMAIN = "#{API_PROTOCOL}://#{API_HOST}"
     end
+    API_PROTOCOL = ENV['COVERALLS_DEVELOPMENT'] ? "http" : "https"
+    API_DOMAIN = "#{API_PROTOCOL}://#{API_HOST}"
 
     API_BASE = "#{API_DOMAIN}/api/v1"
 
